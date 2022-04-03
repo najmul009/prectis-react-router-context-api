6d9ae29c09e9e549fd9f7c2caea3bee544c9e2ad
najmul009 <najmul.tk16@gmail.com>
2022-04-03 16:36:08 +0600

    NavLink vconditiona style added

--- src/Componenets/Header/Header.js
+++ src/Componenets/Header/Header.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 
+
 const Header = () => {
+    
     return (
         <nav className='navbar'>
             <h1>Routing Context API</h1>
-            <div>
-                
+            <div >
+                {/* SHOP */}
+                <NavLink 
+                className={({ isActive }) =>(isActive ? 'active' : 'unactive')} 
+                to="/shop"
+                >
+                Shop
+                </NavLink>
+                {/* Orders  */}
+                <NavLink 
+                className={({ isActive }) =>(isActive ? 'active' : 'unactive')} 
+                to="/orders"
+                >
+                Orders
+                </NavLink>
+                {/* CheckOut  */}
+                <NavLink 
+                className={({ isActive }) =>(isActive ? 'active' : 'unactive')} 
+                to="/checkout"
+                >
+                CheckOut
+                </NavLink>
+                {/* About  */}
+                <NavLink 
+                className={({ isActive }) =>(isActive ? 'active' : 'unactive')} 
+                to="/about"
+                >
+                About
+                </NavLink>
             </div>
         </nav>
     );
